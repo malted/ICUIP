04ebf5c51e2d53eceb4f0852df687f2f012f9108
Malted <malted@malted.dev>
2024-12-13 10:35:11 -0500

    Initial Commit

---
+++ ICU(I)P/ICU_I_PApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICU_I_PApp.swift
+//  ICU(I)P
+//
+//  Created by Ben Dixon on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ICU_I_PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ICU(I)PTests/ICU_I_PTests.swift
@@ -0,0 +1,16 @@
+//
+//  ICU_I_PTests.swift
+//  ICU(I)PTests
+//
+//  Created by Ben Dixon on 13/12/2024.
+//
+
+import Testing
+
+struct ICU_I_PTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
